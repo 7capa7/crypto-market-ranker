4964b88f1b295f762536fdb74e8a9d29ff59eb7e
Patryk <patryk.capanda@windroos.pl>
2026-02-12 16:41:19 +0100

    Base project and docker setup

---
+++ src/main/java/pl/patryk/cryptomarketranker/CryptoMarketRankerApplication.java
@@ -0,0 +1,13 @@
+package pl.patryk.cryptomarketranker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoMarketRankerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoMarketRankerApplication.class, args);
+    }
+
+}
